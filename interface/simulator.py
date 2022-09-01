48654c14a939a744267cefe87514cb00cdf8ca37
Toefinder <43576719+Toefinder@users.noreply.github.com>
2022-09-01 07:36:36 +0200

    Refactor simulator Android message receive
    Make it simpler to do unit tests on simulator method. Also refactor the method
    when receiving Android message so that it can be tested without an Android
    device, and also allow easy modification of the message format later on.

--- interface/simulator.py
+++ interface/simulator.py
@@ -13,6 +13,7 @@ from communication.comms import AlgoClient
 from interface.panel import Panel
 from map.grid import Grid
 from robot.robot import Robot
+import parse
 
 # Set the HEIGHT and WIDTH of the screen
 WINDOW_SIZE = [960, 660]
@@ -46,7 +47,7 @@ class Simulator:
 
         # Initialise 20 by 20 Grid
         self.grid = Grid(20, 20, 20)
-        self.grid.draw_grid(self.screen)
+        # self.grid.draw_grid(self.screen)
         # Outline Grid
         self.grid_surface = self.root.Surface((442, 442))
         self.grid_surface.fill(constants.BLACK)
@@ -73,6 +74,11 @@ class Simulator:
         # Draw the car
         self.car.draw_car()
 
+        # Pattern for messages from ANDROID
+        self.android_pattern = parse.compile("{command:w}/{task:w}/({id:w},{x:d},{y:d},{dir:d})/{obs}")
+        self.obs_pattern = parse.compile("({id:w},{x:d},{y:d},{dir:d})")
+
+    def run(self):
         # Loop until the user clicks the close button.
         done = False
 
@@ -105,7 +111,7 @@ class Simulator:
                     except queue.Empty:  # raised when queue is empty
                         break
                     if isinstance(callback, list):
-                        print(callback)
+                        logging.info("Current callback: \n%s", callback)
                         callback[0](callback[1])
                     else:
                         callback()
@@ -116,9 +122,11 @@ class Simulator:
                     elif event.type == pygame.MOUSEBUTTONDOWN:
                         # User clicks the mouse. Get the position
                         pos = pygame.mouse.get_pos()
-                        if (120 < pos[0] < 560) and (120 < pos[1] < 560):  # if area clicked is within grid
+                        if (constants.min_pixel_pos_x < pos[0] < constants.max_pixel_pos_x) and (
+                            constants.min_pixel_pos_y < pos[1] < constants.max_pixel_pos_y
+                        ):  # if area clicked is within grid
                             self.grid.grid_clicked(pos[0], pos[1])
-                            self.screen.blit(self.grid_surface, (120, 120))  # Redraw the grid outlines
+                            # self.screen.blit(self.grid_surface, (120, 120))  # Redraw the grid outlines
                             self.grid.update_grid(self.screen)  # Update grid if obstacles added
                             self.car.draw_car()  # Redraw the car
 
@@ -149,44 +157,7 @@ class Simulator:
                 txt_split = txt.split("|")
                 source, message = txt_split[0], txt_split[1]
                 if source == "AND":  # From Android
-                    print("Received command from ANDROID")
-                    message_split = message.split("/", 2)
-                    command = message_split[0]
-                    task = message_split[1]
-                    # E.g. message_split = START/EXPLORE/(R,1,1,0)/(00,04,15,-90)/(01,16,17,90)/(02,12,11,180)/(03,07,03,0)/(04,17,04,90)
-                    if command == "START" and task == "EXPLORE":  # Week 8 Task
-                        # Reset first
-                        self.callback_queue.put(self.reset_button_clicked)
-
-                        obstacles = message_split[2]
-                        obstacles_split = obstacles.split("/")
-
-                        # Set robot starting pos
-                        print("Setting robot position...")
-                        robot_starting_pos = obstacles_split[0].strip("()")
-                        robot_params = robot_starting_pos.split(",")
-                        print(robot_params)
-                        robot_x, robot_y, robot_dir = int(robot_params[1]), int(robot_params[2]), int(robot_params[3])
-
-                        self.callback_queue.put(
-                            [self.car.update_robot, [robot_dir, self.grid.grid_to_pixel((robot_x, robot_y))]])
-                        self.callback_queue.put(self.car.redraw_car)
-
-                        # Create obstacles given parameters
-                        print("Creating obstacles...")
-                        for obstacle in obstacles_split[1:]:
-                            obstacle = obstacle[1:-1]
-                            params = obstacle.split(",")
-                            id, grid_x, grid_y, dir = params[0], int(params[1]), int(params[2]), int(params[3])
-                            self.callback_queue.put([self.grid.create_obstacle, [grid_x, grid_y, dir]])
-
-                        # Update grid, start explore
-                        self.callback_queue.put(self.car.redraw_car)
-                        print("[AND] Doing path calculation...")
-                        self.callback_queue.put(self.start_button_clicked)
-
-                    elif command == "START" and task == "PATH":  # Week 9 Task
-                        pass
+                    self.on_receive_android_message(message)
 
                 elif source == "RPI":
                     print("Received command from RPI")
@@ -208,6 +179,82 @@ class Simulator:
             except IndexError:
                 self.comms.send("Invalid command: " + txt)
                 print("Invalid command: " + txt)
+
+    def parse_android_message(self, message: str) -> dict:
+        """Parse message from android into a dictionary for easier manipulation
+
+        Raises:
+            ValueError: if `message` is not in the correct format
+
+        Example:
+            >>> message = "START/EXPLORE/(R,1,1,0)/(00,04,15,-90)/(01,16,17,90)"
+            >>> parse_android_message(message)
+            {"command": "START", "task": "EXPLORE", "robot": {"id": "R", "x": 1, "y": 1, "dir": 0},
+            "obs": [{"id": "00", "x": 4, "y": 15, "dir": -90}, {"id": "01", "x": 16, "y": 17, "dir": 90}]}
+        """
+        data_dict = {}
+        parse_result = self.android_pattern.parse(message)
+        if not parse_result:
+           raise ValueError("Android message is not in the correct format")
+        assert (parse_result["id"] == "R") # robot id must be R
+        data_dict["command"] = parse_result["command"]
+        data_dict["task"] = parse_result["task"]
+        data_dict["robot"] = {key: parse_result[key] for key in ["id", "x", "y", "dir"]}
+        data_dict["obs"] = []
+
+        for obs_data in self.obs_pattern.findall(parse_result["obs"]):
+            data_dict["obs"].append(obs_data.named)
+
+        return data_dict
+
+    def on_receive_android_message(self, message: str):
+        # E.g. message =
+        # START/EXPLORE/(R,1,1,0)/(00,04,15,-90)/(01,16,17,90)/(02,12,11,180)/(03,07,03,0)/(04,17,04,90)
+        logging.info("Received command from ANDROID: %s", message)
+
+        message_data = self.parse_android_message(message)
+        # E.g. message_data = {
+        # 'command': 'START', 'task': 'EXPLORE',
+        # 'robot': {'id': 'R', 'x': 1, 'y': 1, 'dir': 0},
+        # 'obs': [{'id': '00', 'x': 4, 'y': 15, 'dir': -90},
+        #         {'id': '01', 'x': 16, 'y': 17, 'dir': 90},
+        #         {'id': '02', 'x': 12, 'y': 11, 'dir': 180},
+        #         {'id': '03', 'x': 7, 'y': 3, 'dir': 0},
+        #         {'id': '04', 'x': 17, 'y': 4, 'dir': 90}
+        #         ]
+        # }
+
+        command = message_data["command"]
+        task = message_data["task"]
+
+
+        if command == "START" and task == "EXPLORE":  # Week 8 Task
+            # Reset first
+            self.callback_queue.put(self.reset_button_clicked)
+
+            # Set robot starting pos
+            robot_params = message_data['robot']
+            logging.info("Setting robot position: %s", robot_params)
+            robot_x, robot_y, robot_dir = int(robot_params["x"]), int(robot_params["y"]), int(robot_params["dir"])
+
+            self.callback_queue.put([self.car.update_robot, [robot_dir, self.grid.grid_to_pixel((robot_x, robot_y))]])
+            self.callback_queue.put(self.car.redraw_car)
+
+            # Create obstacles given parameters
+            logging.info("Creating obstacles...")
+            for obstacle in message_data["obs"]:
+                logging.info("Obstacle: %s", obstacle)
+                id, grid_x, grid_y, dir = obstacle["id"], int(obstacle["x"]), int(obstacle["y"]), int(obstacle["dir"])
+                self.callback_queue.put([self.grid.create_obstacle, [grid_x, grid_y, dir]])
+
+            # Update grid, start explore
+            self.callback_queue.put(self.car.redraw_car)
+
+            logging.info("[AND] Doing path calculation...")
+            self.callback_queue.put(self.start_button_clicked)
+
+        elif command == "START" and task == "PATH":  # Week 9 Task
+            pass
 
     def reprint_screen_and_buttons(self):
         self.screen.fill(constants.GRAY)
@@ -290,3 +337,27 @@ class Simulator:
     def reset_button_clicked(self):
         self.grid.reset(self.screen)
         self.car.reset()
+
+if __name__ == "__main__":
+    # Set info logging mode
+    logging.basicConfig(level=logging.INFO)
+
+    x = Simulator()
+
+    # Test the method to parse Android messages
+    message = "START/EXPLORE/(R,1,1,0)/(00,04,15,-90)/(01,16,17,90)/(02,12,11,180)/(03,07,03,0)/(04,17,04,90)"
+    assert x.parse_android_message(message) == {
+        'command': 'START', 'task': 'EXPLORE',
+        'robot': {'id': 'R', 'x': 1, 'y': 1, 'dir': 0},
+        'obs': [{'id': '00', 'x': 4, 'y': 15, 'dir': -90},
+                {'id': '01', 'x': 16, 'y': 17, 'dir': 90},
+                {'id': '02', 'x': 12, 'y': 11, 'dir': 180},
+                {'id': '03', 'x': 7, 'y': 3, 'dir': 0},
+                {'id': '04', 'x': 17, 'y': 4, 'dir': 90}
+                ]
+        }
+
+    # Test the threading without Android connected
+    thread = threading.Thread(target=lambda: x.on_receive_android_message(message))
+    thread.start()
+    x.run()
