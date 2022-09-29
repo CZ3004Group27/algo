3cfb1b6f4a054c8bec0da09d8194d181dba63660
Toefinder <43576719+Toefinder@users.noreply.github.com>
2022-09-29 06:10:01 +0200

    Do not send RPi back the invalid command
    Since the wifi server uses TCP, it is almost guaranteed that the message
    sent over will be consistent. According to RPi team this sendback is not
    necessary.

--- fastestalgo/__main__.py
+++ fastestalgo/__main__.py
@@ -38,7 +38,6 @@ class Week9Task:
                     self.on_receive_image_taken_message(message_data)
 
             except (IndexError, ValueError) as e:
-                self.comms.send("Invalid command: " + txt)
                 print("Invalid command: " + txt)
 
     def on_receive_image_taken_message(self, data_dict: dict):
--- mdpalgo/interface/simulator.py
+++ mdpalgo/interface/simulator.py
@@ -195,7 +195,6 @@ class Simulator:
                     self.on_receive_image_taken_message(message_data)
 
             except (IndexError, ValueError) as e:
-                self.comms.send("Invalid command: " + txt)
                 print("Invalid command: " + txt)
 
     def on_receive_image_taken_message(self, data_dict: dict):
